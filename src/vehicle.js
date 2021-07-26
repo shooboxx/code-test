7c504ffb13e4a2d108a7b525bc919582dd8c1b19
Stefano Hepburn <stefanohepburn@gmail.com>
2021-07-26 15:57:26 -0400

    Added temp database

--- src/vehicle.js
+++ src/vehicle.js
@@ -1,15 +1,43 @@
 getVehicle = (req, res) => {
-    res.send({
-        "type": "Car",
-        "milage": 3000,
-        "hybrid": false
-    })
+    res.send(vehicleRepo)
 }
 addVehicle = (req, res) => {
-    res.send({
-        "car": "chevy",
-        "color": "black"
-    })
+    const vehicles = []
+    
+    for (let i = 0; i < req.body.length; i++) {  
+        const newVehicle = {"type": req.body[i].type, "milage": req.body[i].milage, "hybrid": req.body[i].hybrid};
+        vehicleRepo.push(newVehicle)
+        vehicles.push(newVehicle)
+    }
+    res.send(vehicles)
 }
 
 module.exports = { getVehicle, addVehicle } 
+
+let vehicleRepo = [
+    {
+        "type": "Car",
+        "milage": 3000,
+        "hybrid": false
+    },
+    {
+        "type": "Truck",
+        "milage": 24000,
+        "hybrid": true
+    },
+    {
+        "type": "Bus",
+        "milage": 30,
+        "hybrid": false
+    },
+    {
+        "type": "Car",
+        "milage": 80000,
+        "hybrid": false
+    },
+    {
+        "type": "Car",
+        "milage": 58000,
+        "hybrid": false
+    }
+]
