00dcbb6550379bc860cd16118accd45880ce91f9
Stefano Hepburn <stefanohepburn@gmail.com>
2021-07-26 16:18:36 -0400

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,38 +1,15 @@
 const express = require( "express" );
-const bodyParser = require('body-parser');
-
 
 const app = express();
 const PORT = 5000; // default port to listen
 const { getVehicle, addVehicle } = require('./vehicle');
 
-const jsonParser = bodyParser.json();
+app.use(express.json());
 // define a route handler for the default home page
 app.get( "/", getVehicle);
-app.post( "/", jsonParser, addVehicle);
+app.post( "/", addVehicle);
 
 // start the Express server
 app.listen( PORT, () => {
     console.log( `server started at http://localhost:${ PORT }` );
 } );
-
-
-
-// // Testing database
-// const { Client } = require('pg');
-
-// const client = new Client({
-// //   connectionString: process.env.DATABASE_URL,
-//   connectionString: `postgres://gblnzulcumvlag:93b42a2de09b117b290a5d321c34a712f14086d1572ea3f42278a0d8b8c235cf@ec2-52-200-119-0.compute-1.amazonaws.com:5432/d8c31nmisis38f`,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-// client.connect();
-
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//   if (err) throw err;
-//     if (!err) console.log('it worked')
-//   client.end();
-// });
